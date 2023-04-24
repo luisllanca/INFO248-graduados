7ce432ac02ad4d6df1cbbf94c7ed674d34bb2fe3
xNanoEngine <xnanoengine@gmail.com>
2023-04-24 19:51:57 -0400

    Rutas ordenadas y cambios

--- Readme.md
+++ Readme.md
@@ -1,14 +1,19 @@
-## Intrucciones ejecución
-
-1. Instalar modulos con `npm install`
-2. Copiar y pegarel archivo template.env, y luego renombrar a `.env`
-3. Para Correr el proyecto escribir `npm run dev`
--- Funcionalidad filtar estudiante (retorna la informacion del estudiante)
-4. ir a la ruta http://localhost:8080/gestion/estudiantes/1
-5. el valor "1" filtra por id de estudiante, si lo cambian muestran otro estudiante.
--- Funcionalidad ver comprobantes  (retorna los comprobantes de un estudiante en especifico)
-6. ir a la ruta http://localhost:8080/gestion/estudiantes/1/comprobantes
-7. el valor "1" filtra por id de estudiante, si lo cambian muestran otros comprobantes de otro estudiante.
--- Funcionalidad revisarHistorialPagos  (retorna los pagos de un estudiante en especifico)
-6. ir a la ruta http://localhost:8080/gestion/estudiantes/1/pagos
-7. el valor "1" filtra por id de estudiante, si lo cambian muestran otros pagos de otro estudiante.
+# INFO248-graduados
+Este grupo está conformado por:  
+1. Isidora Hernández  
+2. Luis Llanca  
+3. Felipe Prieto
+4. Benjamín Cifuentes  
+5. Alejandro Villagrán  
+6. Rodolfo Seguel  
+7. Hernán Cornejo
+8. Cristián Cárdenas
+9. Tomás Manriquez
+10. Sebastián Paredes
+11. Matías Martinez  
+  
+  
+  
+## [Link de primera versión de la arquitectura](https://docs.google.com/document/d/1ofz-5r6KL__B43xaDMccg2fA5wPkNCBAw3nR-aEFGr0/edit?usp=sharing)
+## [Discord del proyecto de graduados](https://discord.gg/GAxNfxkA)  
+## [Arquitectura, diseño detallado](https://docs.google.com/document/d/1SVuIvZKJtWngE9ddaoRpsWZmH2drfS-7fVUga__5k58/edit?usp=sharing)  
--- src/server/models/server.ts
+++ src/server/models/server.ts
@@ -1,44 +1,16 @@
-import express, { Application, Request, Response } from "express";
+import express, { Application } from "express";
 import cors from "cors";
-import { Estudiante } from "../../Classes/models/Estudiante";
-import { data } from "../../seed/data";
-import { Gestion } from "../../Classes/models/Gestion";
+import rutas from "../../routes/GestionRoutes";
 
 class Server {
   private app: Application;
   private port: string;
-  private gestion: Gestion;
-  private estudiantes: Array<Estudiante>;
 
   constructor() {
     this.app = express();
     this.port = process.env.PORT || "9000";
-    this.estudiantes = this.createEstudiante();
-    this.gestion = new Gestion(
-      "gestion@em.cl",
-      "123soygestion",
-      this.estudiantes
-    );
     this.middlewares();
     this.routes();
-  }
-
-  createEstudiante(): Array<Estudiante> {
-    const est: Array<Estudiante> = [];
-    for (let i = 0; i < data.length; i++) {
-      const estudiante1 = new Estudiante(
-        data[i].id,
-        data[i].nombre,
-        data[i].email,
-        data[i].password,
-        data[i].rut,
-        data[i].carrera,
-        data[i].comprobante,
-        data[i].becas
-      );
-      est.push(estudiante1);
-    }
-    return est;
   }
 
   middlewares() {
@@ -47,30 +19,7 @@ class Server {
   }
 
   routes() {
-    this.app.get("/gestion/estudiantes/:id", (req: Request, res: Response) => {
-      const { id } = req.params;
-      res.json({
-        Estudiantes: this.gestion.filtrarEstudiantes(+id),
-      });
-    });
-    this.app.get(
-      "/gestion/estudiantes/:id/comprobantes",
-      (req: Request, res: Response) => {
-        const { id } = req.params;
-        res.json({
-          Comprobantes: this.gestion.revisarComprobantesEstudiante(+id),
-        });
-      }
-    );
-    this.app.get(
-      "/gestion/estudiantes/:id/pagos",
-      (req: Request, res: Response) => {
-        const { id } = req.params;
-        res.json({
-          Pagos: this.gestion.revisarHistorialPagos(+id),
-        });
-      }
-    );
+    this.app.use("/gestion", rutas);
   }
 
   listen() {
