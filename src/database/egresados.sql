4e739fed119ed55871d531feca67bca6756e52e4
xNanoEngine <xnanoengine@gmail.com>
2023-05-15 20:43:29 -0400

    base de datos actualizada

--- src/database/egresados.sql
+++ src/database/egresados.sql
@@ -1,6 +1,6 @@
 -- --------------------------------------------------------
 -- Host:                         127.0.0.1
--- Versión del servidor:         10.4.27-MariaDB - mariadb.org binary distribution
+-- Versión del servidor:         10.4.28-MariaDB - mariadb.org binary distribution
 -- SO del servidor:              Win64
 -- HeidiSQL Versión:             12.4.0.6659
 -- --------------------------------------------------------
@@ -14,65 +14,13 @@
 /*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
 /*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
 
+-- La exportación de datos fue deseleccionada.
 
--- Volcando estructura de base de datos para egresados
-CREATE DATABASE IF NOT EXISTS `egresados` /*!40100 DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci */;
-USE `egresados`;
+-- La exportación de datos fue deseleccionada.
 
--- Volcando estructura para tabla egresados.beca
-CREATE TABLE IF NOT EXISTS `beca` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `tipo` varchar(50) NOT NULL,
-  `monto` int(11) NOT NULL,
-  `fechaAsi` datetime NOT NULL,
-  `fechaExp` datetime NOT NULL,
-  `id_estudiante` int(11) NOT NULL,
-  PRIMARY KEY (`id`),
-  KEY `id_estudiante` (`id_estudiante`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+-- La exportación de datos fue deseleccionada.
 
--- Volcando datos para la tabla egresados.beca: ~0 rows (aproximadamente)
-
--- Volcando estructura para tabla egresados.comprobante
-CREATE TABLE IF NOT EXISTS `comprobante` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `fecha` datetime NOT NULL,
-  `tipo` varchar(50) NOT NULL,
-  `monto` int(11) NOT NULL,
-  `img` varchar(50) NOT NULL,
-  `id_estudiante` varchar(50) NOT NULL,
-  PRIMARY KEY (`id`),
-  KEY `id_estudiante` (`id_estudiante`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
-
--- Volcando datos para la tabla egresados.comprobante: ~0 rows (aproximadamente)
-
--- Volcando estructura para tabla egresados.estudiante
-CREATE TABLE IF NOT EXISTS `estudiante` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `nombre` varchar(50) NOT NULL,
-  `email` varchar(50) NOT NULL,
-  `password` varchar(50) NOT NULL,
-  `rut` int(9) NOT NULL,
-  `carrera` varchar(50) NOT NULL,
-  `id_profesor` int(11) NOT NULL,
-  PRIMARY KEY (`id`),
-  KEY `id_profesor` (`id_profesor`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
-
--- Volcando datos para la tabla egresados.estudiante: ~0 rows (aproximadamente)
-
--- Volcando estructura para tabla egresados.gestion
-CREATE TABLE IF NOT EXISTS `gestion` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `nombre` varchar(50) NOT NULL,
-  `email` varchar(50) NOT NULL,
-  `password` varchar(50) NOT NULL,
-  `rut` int(9) NOT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
-
--- Volcando datos para la tabla egresados.gestion: ~0 rows (aproximadamente)
+-- La exportación de datos fue deseleccionada.
 
 /*!40103 SET TIME_ZONE=IFNULL(@OLD_TIME_ZONE, 'system') */;
 /*!40101 SET SQL_MODE=IFNULL(@OLD_SQL_MODE, '') */;
