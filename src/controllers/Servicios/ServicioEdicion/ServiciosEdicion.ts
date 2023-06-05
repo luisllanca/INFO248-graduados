52c57ec0fcd5586b00fd157506a92ca2a4cd5a8a
Matías Martínez <83073528+xNanoEngine@users.noreply.github.com>
2023-06-04 21:11:31 -0400

    old deleted

--- src/controllers/Servicios/ServicioEdicion/ServiciosEdicion.ts
+++
@@ -1,7 +0,0 @@
-import type { Estudiante } from "../../Usuario/Usuario/Estudiante";
-import type { Beca } from "../../Servicios/ServicioFinanciero/Beca";
-
-export interface ServiciosEdicion {
-    asignarBeca(est: Estudiante, bec: Beca): void;
-    eliminarBeca(est: Estudiante, bec: Beca): void;
-}
--- src/controllers/Servicios/ServicioVisualizacion/ServiciosVisualizacion.ts
+++
@@ -1,8 +0,0 @@
-import { Estudiante } from "../../Usuario/Usuario/Estudiante"
-
-export interface ServiciosVisualizacion {
-    verMatriculasEstudiante(estudiante: Estudiante): void
-    revisarComprobantesEstudiante(estudiante: Estudiante): void
-    revisarHistorialPagos(estudiante: Estudiante): void
-    verBecasAsignadas(estudiante: Estudiante): void
-}
